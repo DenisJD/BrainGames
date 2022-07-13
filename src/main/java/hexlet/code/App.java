b56f837946b60d20f4a1a610d4d1492e8918daf7
DenisJD <t.denisjd@gmail.com>
2022-07-13 17:51:36 +0300

    fix App file

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -7,55 +7,26 @@ import hexlet.code.games.Progression;
 import hexlet.code.games.Prime;
 
 import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class App {
     public static void main(String[] args) {
-        getFirstMessage();
-        getChooseGameMessage();
-        chooseGame();
-    }
-
-    public static void getFirstMessage() {
         System.out.println("Please enter the game number and press Enter.");
-    }
-
-    public static void getChooseGameMessage() {
         System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
-    }
-
-    public static void chooseGame() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Your choice: ");
         String choice = scanner.nextLine();
         switch (choice) {
-            case "1" -> {
-                Engine engine = new Engine(scanner, ThreadLocalRandom.current());
-                new Cli(engine).greetings();
+            case "1" -> Cli.greetings();
+            case "2" -> Even.startGame();
+            case "3" -> Calc.startGame();
+            case "4" -> GCD.startGame();
+            case "5" -> Progression.startGame();
+            case "6" -> Prime.startGame();
+            case "0" -> {
             }
-            case "2" -> {
-                Engine engine = new Engine(scanner, ThreadLocalRandom.current());
-                new Even(engine).startGame();
-            }
-            case "3" -> {
-                Engine engine = new Engine(scanner, ThreadLocalRandom.current());
-                new Calc(engine).startGame();
-            }
-            case "4" -> {
-                Engine engine = new Engine(scanner, ThreadLocalRandom.current());
-                new GCD(engine).startGame();
-            }
-            case "5" -> {
-                Engine engine = new Engine(scanner, ThreadLocalRandom.current());
-                new Progression(engine).startGame();
-            }
-            case "6" -> {
-                Engine engine = new Engine(scanner, ThreadLocalRandom.current());
-                new Prime(engine).startGame();
-            }
-            default -> {
-            }
+            default -> System.out.println("'" + choice + "'" + " is wrong game number :(");
         }
         scanner.close();
     }
 }
+
