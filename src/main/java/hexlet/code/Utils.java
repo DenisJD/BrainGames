df50a15b4b6bbde5b3c4cc6037730183b0efd0f4
DenisJD <t.denisjd@gmail.com>
2022-07-15 12:11:56 +0300

    save one method of find random number

--- src/main/java/hexlet/code/Utils.java
+++ src/main/java/hexlet/code/Utils.java
@@ -3,12 +3,6 @@ package hexlet.code;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Utils {
-    public static final int MAX_RANDOM_NUMBER = 100;
-
-    public static int getRandomNumber() {
-        return ThreadLocalRandom.current().nextInt(MAX_RANDOM_NUMBER);
-    }
-
     /**
      * @param from from inclusive
      * @param to   to exclusive
